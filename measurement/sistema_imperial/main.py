46c5cd3f8e0904f1d20e0e21acbd89d5d7975287
Nycollas <nhhenich@gmail.com>
2024-11-13 21:13:55 -0300

    main inicial e sistema imperial

---
+++ measurement/sistema_imperial/main.py
@@ -0,0 +1,7 @@
+def main():
+    print('Conversor de unidades métricas - Imperial')
+    source_unit = input('[mm] - [cm] - [m] - [km]\n  ').lower()
+    destination_unit = input('[mm] - [cm] - [m] - [km]\n').lower()
+    value_inital = float(input('Insira o valor que será convertido: '))
+
+main()
