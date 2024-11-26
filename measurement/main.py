658f9e8f894578066b17f84cc6123a794fa37726
Nycollas <nhhenich@gmail.com>
2024-11-26 07:42:32 -0300

    testes e mais testes

--- measurement/main.py
+++ measurement/main.py
@@ -17,24 +17,49 @@ def InterfaceUsuario():
             print('Sistema de destino desconhecido.')
             continue
 
-        unidade_origem = input(f'Digite a unidade de origem: \n{FatoresMetrosImperial} | \n{FatoresMetrosMetrico}: ')
-        unidade_destino = input(f'Digite a unidade de destino: \n{FatoresMetrosImperial} | \n{FatoresMetrosMetrico}: ')
+        if sistema_origem == 'imperial':
+            unidades_origem = FatoresMetrosImperial
+        else:
+            unidades_origem = FatoresMetrosMetrico
 
+        unidade_origem = input(f'Digite a unidade de origem: \n{unidades_origem}: ')
+        if unidade_origem not in unidades_origem:
+            print(f'Unidade de origem {unidades_origem} não é válida no sistema {sistema_origem}.')
+            continue
         
+        
+        if sistema_destino == 'imperial':
+            unidades_destino = FatoresMetrosImperial
+        else:
+            unidades_destino = FatoresMetrosMetrico
+        
+        unidade_destino = input(f'Digite a unidade de destino: \n{unidades_destino} ')
+        if unidade_destino not in unidades_destino:
+            print(f'Unidade de destino {unidades_origem} não é válida no sistema {sistema_destino}.')
+            continue
 
         try:
             valor = float(input(f'Digite o valor a ser convertido de {unidade_origem} | {unidade_destino}: '))
-            print(f'Entrada: {valor} {unidade_origem} | {unidade_destino}')
+            print(f'Entrada: {valor} | {unidade_origem} | {unidade_destino}')
 
-            resultado = ConverterEntreSistemas(valor, unidade_origem, unidade_destino, sistema_origem, sistema_destino)
+            
+            resultado = ConverterEntreSistemas(
+                valor=valor, 
+                unidade_origem=unidade_origem,
+                unidade_destino=unidade_destino,
+                sistema_origem=sistema_origem,
+                sistema_destino=sistema_destino
+            )
+            
+            # resultado = ConverterEntreSistemas(unidade_origem='cm', unidade_destino='m', sistema_origem='métrico', sistema_destino='métrico', valor=10)
+            print(f"Debug: {valor}, {unidade_origem}, {unidade_destino}, {sistema_origem}, {sistema_destino}")
             print(f'\nResultado = {valor}{unidade_origem} é igual a {resultado}{unidade_destino}.\n')
-        
     
         except ValueError:
             print('Valor inválido.')
             continue
 
-        except ValueError as e:
+        # except Exception as e:
             print(f'Erro: {e}')
             continue
 
